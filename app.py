f80673aeed3cd252f6e28fc7e1e64f32137c5a58
Ay-source <ayomidesamuel04@gmail.com>
2022-09-18 00:09:34 +0100

    Fixed issue with sessions

--- app.py
+++ app.py
@@ -8,38 +8,60 @@ app = Flask(__name__)
 setup(app)
 Session(app)
 
+
+def check_session():
+    try:
+        if session['name']:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+
+
 #Route handler for the sign in page
 @app.route("/sign_in")
 def sign_in():
     form = User()
-    try:
-        if request.args.get("username", "") in session['name']:
-            flash("You're logged in")
-            return redirect(url_for("home"))
-    except:
-        pass
+    username=request.args.get("username", ".")
+
+    #Check if user session exists
+    state = check_session()
+    if state:
+        flash("You are logged in.")
+        return redirect(url_for("home"))
+
     return render_template("sign_in.html", form=form)
 
 @app.route("/sign_in", methods=["POST"])
 def post_sign_in():
+    #Form Validation
     form = User(request.form)
     username, password, permission = check_login(form, request, True)
-    user = Users.query.filter_by(username=username).first()
+    
+    #Check if password matches with database password
+    user = Users.query.filter_by(username=username).one_or_none()
+    if user == None:
+        flash("Username/Password doesn't match")
+        return redirect(url_for("sign_in"))
     if password == user.password:
         session['name'] = username
         flash("You're Logged in!")
         return redirect(url_for("home"))
-    return redirect(url_for("sign_in"))
+    else:
+        flash("Username/Password doesn't match")
+        return redirect(url_for("sign_in"))
 
 @app.route("/sign_up")
 def sign_up():
     form = User()
-    try:
-        if request.args.get("username", "") in session['name']:
-            flash("You're logged in")
-            return redirect(url_for("home"))
-    except:
-        pass
+    
+    #Check if user session exists
+    state = check_session()
+    if state:
+        flash("You are logged in.")
+        return redirect(url_for("home"))
     return render_template("sign_up.html", range=range, permissions=["sell:books", "read:books"], form=form)
 
 @app.route("/sign_up", methods=["POST"])
@@ -58,7 +80,6 @@ def post_sign_up():
     except Exception as e:
         new_user.rollback()
         flash("An error occured!")
-        print(e)
         return redirect(url_for("sign_up"))
     return redirect(url_for("sign_in"))
 
@@ -70,27 +91,26 @@ def root():
 #Handler for the home page
 @app.route("/home")
 def home():
-    try:
-        if request.args.get("username", "") in session['name']:
-            flash("You're logged in")
-            return redirect(url_for("home"))
-    except:
-        flash("You're not logged in")
-        return redirect(url_for("root"))
+    #Check if user session exists
+    state = check_session()
+    if not state:
+        flash("You are not logged in.")
+        return redirect(url_for("sign_in"))
+
     return render_template("profile.html")
 
 @app.route("/sign_out")
 def sign_out():
-    try:
-        if request.form.get("username", "") not in session['name']:
-            flash("please log in")
-            return redirect(url_for("sign_in"))
-    except:
-        flash("You're not logged in")
-        return redirect(url_for("home"))
+    #Check if user session exists
+    state = check_session()
+    if not state:
+        flash("You are not logged in.")
+        return redirect(url_for("sign_in"))
+
+    #Detach session
     session["name"] = None
     flash("You've been logged out.")
-    return redirect(url_for("sign_in"))
+    return redirect(url_for("root"))
 
 """
 #Handler for the profile page
